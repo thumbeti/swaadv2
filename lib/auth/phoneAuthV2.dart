69c242b039f42f80536fbbfb14c167606d577252
thumbeti <thumbeti@gmail.com>
2019-11-16 21:16:37 -0600

    Added 2nd phone auth screen

---
+++ lib/auth/phoneAuthV2.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:swaadv2/MenuHome.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class PhoneAuth extends StatefulWidget {
+  @override
+  _PhoneAuthState createState() => _PhoneAuthState();
+}
+
+class _PhoneAuthState extends State<PhoneAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(builder: (BuildContext context) {
+        return ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            _PhoneSignInSection(Scaffold.of(context)),
+          ],
+        );
+      }),
+    );
+  }
+
+  // Example code for sign out.
+  void _signOut() async {
+    await _auth.signOut();
+  }
+}
+
+class _PhoneSignInSection extends StatefulWidget {
+  _PhoneSignInSection(this._scaffold);
+
+  final ScaffoldState _scaffold;
+  @override
+  State<StatefulWidget> createState() => _PhoneSignInSectionState();
+}
+
+class _PhoneSignInSectionState extends State<_PhoneSignInSection> {
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _smsController = TextEditingController();
+
+  String _message = '';
+  String _verificationId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: SizedBox(
+            height: 300.0,
+            child: Image(
+              alignment: Alignment.center,
+              image: AssetImage("assets/swaad_logo.jpg"),
+              //fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        SizedBox(height: 45.0),
+        TextFormField(
+          controller: _phoneNumberController,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            labelText: 'Phone number (+x xxx-xxx-xxxx)',
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          ),
+          validator: (String value) {
+            if (value.isEmpty) {
+              return 'Phone number (+x xxx-xxx-xxxx)';
+            }
+            return null;
+          },
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _verifyPhoneNumber();
+            },
+            child: const Text('Verify phone'),
+          ),
+        ),
+        TextField(
+          controller: _smsController,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              labelText: 'Verification code',
+              border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _signInWithPhoneNumber();
+            },
+            child: const Text('Sign in'),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            _message,
+            style: TextStyle(color: Colors.red),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Example code of how to verify phone number
+  void _verifyPhoneNumber() async {
+    setState(() {
+      _message = '';
+    });
+    final PhoneVerificationCompleted verificationCompleted =
+        (AuthCredential phoneAuthCredential) {
+      _auth.signInWithCredential(phoneAuthCredential);
+      setState(() {
+        _message = 'Received phone auth credential: $phoneAuthCredential';
+      });
+    };
+
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException authException) {
+      setState(() {
+        _message =
+            'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}';
+      });
+    };
+
+    final PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      widget._scaffold.showSnackBar(const SnackBar(
+        content: Text('Please check your phone for the verification code.'),
+      ));
+      _verificationId = verificationId;
+    };
+
+    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      _verificationId = verificationId;
+    };
+
+    await _auth.verifyPhoneNumber(
+        phoneNumber: _phoneNumberController.text,
+        timeout: const Duration(seconds: 5),
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+  }
+
+  // Example code of how to sign in with phone.
+  void _signInWithPhoneNumber() async {
+    final AuthCredential credential = PhoneAuthProvider.getCredential(
+      verificationId: _verificationId,
+      smsCode: _smsController.text,
+    );
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _message = 'Successfully signed in, uid: ' + user.uid;
+        print(_message);
+        widget._scaffold.showSnackBar(const SnackBar(
+          content: Text('Successfully signed in..SIVA'),
+        ));
+        Navigator.of(context).push(
+          MaterialPageRoute<void>(builder: (_) => MenuHome()),
+        );
+      } else {
+        _message = 'Sign in failed';
+      }
+    });
+  }
+}
